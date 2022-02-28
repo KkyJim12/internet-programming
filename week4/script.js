cee6290bb028d7799217d7354cd2203db5e41273
Piyakarn Nimmakulvirut (Jimmy) <jirakarnjim1@gmail.com>
2022-02-28 17:09:36 +0000

    Update week4

--- week4/script.js
+++ week4/script.js
@@ -1,8 +1,23 @@
 const print5 = () => {
-    for(let i=1; i<6; i++) {
-        alert(i);
-    }
-}
+  for (let i = 1; i < 6; i++) {
+    console.log(i);
+  }
+};
 
 print5();
 print5();
+
+const changeHeading = () => {
+  document.getElementById("heading").firstChild.nodeValue = "New Heading";
+};
+
+const changeContent = () => {
+  document.getElementById("content").firstChild.nodeValue = "New Paragraph !!";
+};
+
+const loadFunction = () => {
+  document.getElementById("heading").addEventListener("click", changeHeading);
+  document.getElementById("content").addEventListener("click", changeContent);
+};
+
+document.addEventListener("DOMContentLoaded", loadFunction);
